ceebc8b761b381845199f24554ff0f899f597fab
luchux <lucianoguasco@gmail.com>
2014-12-06 18:28:22 +1100

    added helper for momentjs from method

--- client/helpers/stringHelpers.js
+++ client/helpers/stringHelpers.js
@@ -6,3 +6,8 @@ Template.registerHelper('summarize', function(string) {
   var cleanString = _(string).stripTags();
   return _(cleanString).truncate(140);
 });
+
+
+Template.registerHelper('momentFrom', function (date) {
+    return moment(date).from(Date.now())
+})
